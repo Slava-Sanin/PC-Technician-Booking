4398d0000c6b3836f1a86fdf83e190d504a39eed
Viacheslav Sanin <slava.sanin@gmail.com>
2025-12-23 15:07:50 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/pc-technician-booking/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
